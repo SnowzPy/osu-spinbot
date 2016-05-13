8daa77de89c8f92a93e54405fee719858038a4e4
shavitush <djshavit@gmail.com>
2016-05-14 01:44:54 +0300

    I use SetCursorPos() now

--- spinbot/hax.h
+++ spinbot/hax.h
@@ -5,17 +5,3 @@
 #define TITLE "Steam"
 
 void Start(void *cancer);
-
-int SendMouse(int x, int y)
-{
-	INPUT InputData;
-	InputData.type = INPUT_MOUSE;
-	InputData.mi.dx = x;
-	InputData.mi.dy = y;
-	InputData.mi.mouseData = NULL;
-	InputData.mi.dwFlags = MOUSEEVENTF_MOVE;
-	InputData.mi.time = NULL;
-	InputData.mi.dwExtraInfo = GetMessageExtraInfo();
-
-	return SendInput(1, &InputData, sizeof(InputData));
-}
